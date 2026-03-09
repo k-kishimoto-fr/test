a421a31f1f7a8bf2fef2c27b317903b063049fa8
k-kishimoto-fr <kazuya.kishimoto@futurerays.biz>
2026-03-09 17:35:41 +0900

    iro iro

--- 260309/iperf3/P5/iperf3_test.sh
+++ 260309/iperf3/P5/iperf3_test.sh
@@ -9,6 +9,6 @@ typeset -r MYNAME=${MYFILE%.*} # スクリプト名(拡張子なし)
 
 for (( i=1; i<=RUNS; i++)); do
     OFILE=${MYNAME}-${i}.txt
-    stdbuf -oL iperf3 -c "$SERVER" -t 10 -i 1 -P 5 --timestamps="%Y-%m-%d %H:%M:%S"  --forceflush | tee ${OFILE}
+    stdbuf -oL iperf3 -c "$SERVER" -t 10 -i 1 -u -b 9G  --timestamps="%Y-%m-%d %H:%M:%S"  --forceflush | tee ${OFILE}
     sleep $INTERVAL
 done
